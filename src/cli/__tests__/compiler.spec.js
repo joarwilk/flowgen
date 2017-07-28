d3eec57ee2d4a6bed3fa104332fdef81a1571493
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2017-07-28 00:59:11 -0300

    add tests to compiler

---
+++ src/cli/__tests__/compiler.spec.js
@@ -0,0 +1,21 @@
+/* @flow */
+import compiler from '../compiler';
+
+it('should handle maybe & nullable type', () => {
+  const result = compiler.compileDefinitionString('let a: string | null | undefined');
+
+  expect(result).toMatchSnapshot();
+});
+
+it('should handle bounded polymorphism', () => {
+  const ts = `
+    function fooGood<T extends { x: number }>(obj: T): T {
+      console.log(Math.abs(obj.x));
+      return obj;
+    }
+  `;
+
+  const result = compiler.compileDefinitionString(ts);
+
+  expect(result).toMatchSnapshot();
+});
