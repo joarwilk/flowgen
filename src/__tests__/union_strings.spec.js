ccfbeaa189b14ee70f675601c731bf3c7cb6a88b
Orta Therox <orta.therox@gmail.com>
2018-02-21 12:54:02 -0500

    An example commit of adding a test for a dts

---
+++ src/__tests__/union_strings.spec.js
@@ -0,0 +1,14 @@
+import compiler from '../cli/compiler';
+import beautify from '../cli/beautifier';
+
+it('should handle union strings', () => {
+  const ts = `
+  interface MyObj {
+    state?: "APPROVED" | "REQUEST_CHANGES" | "COMMENT" | "PENDING"
+  }
+`;
+
+  const result = compiler.compileDefinitionString(ts);
+  
+  expect(beautify(result)).toMatchSnapshot()
+});
