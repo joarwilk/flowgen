6a0d8624be384f4d440f9d93fec35ff6271f2747
Tal <tshani@gmail.com>
2018-05-24 07:49:53 +0300

    Replacing js-beautify with prettier

--- src/cli/beautifier.js
+++ src/cli/beautifier.js
@@ -1,22 +1,6 @@
 /* @flow */
-import { js_beautify } from "js-beautify";
+import prettier from "prettier";
 
 export default function beautify(str: string) {
-  const formatted: string = js_beautify(str);
-
-  // no idea how to disable this correction in js-beautify
-  const corrected = formatted
-    .split(" ? : ")
-    .join("?: ")
-    // .split('(\n            ').join('(')
-    .split("declare\n")
-    .join("declare ")
-    .split("declare     ")
-    .join("declare ")
-    .split(" < ")
-    .join("<")
-    .split(" > ")
-    .join(">");
-
-  return corrected;
+  return prettier.format(str);
 }
