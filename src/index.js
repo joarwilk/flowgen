d69840db66dfe72240b3449b9bac192127e62999
vikr01 <vik120@icloud.com>
2018-11-24 21:47:32 -0800

    fix exports to match readme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import compiler from "./cli/compiler";
-import beautify from "./cli/beautifier";
+import compiler from './cli/compiler';
+import beautify from './cli/beautifier';
+
+export {default as compiler} from './cli/compiler';
+
+export {default as beautify} from './cli/beautifier';
 
 export default {
   beautify,
