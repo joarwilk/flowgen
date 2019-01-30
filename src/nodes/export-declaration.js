0234f4dcd8a17511f6da94809a53234ce454fbb5
andretshurotshka <andwebar@gmail.com>
2019-01-31 02:55:12 +0500

    Add export declaration

---
+++ src/nodes/export-declaration.js
@@ -0,0 +1,23 @@
+/* @flow */
+import type { RawNode } from "./node";
+import Node from "./node";
+
+import printers from "../printers";
+
+export default class ExportDeclaration extends Node {
+  constructor(node: RawNode) {
+    super(node);
+  }
+
+  print() {
+    if (this.raw.exportClause) {
+      return `export {
+        ${this.raw.exportClause.elements.map(node => {
+          return `${node.name.text}`;
+        })}
+      };`;
+    } else {
+      return `export * from '${this.raw.moduleSpecifier.text}';`;
+    }
+  }
+}
