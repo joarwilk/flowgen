ffb594e8114302aa2b8e860627c36e9eef50b943
andretshurotshka <andwebar@gmail.com>
2019-01-31 16:27:03 +0500

    Add function exports spec

--- src/__tests__/function_exports.spec.js
+++ src/__tests__/function_exports.spec.js
@@ -1,9 +1,15 @@
-import {compiler, beautify} from "..";
+import { compiler, beautify } from "..";
 
 it("should handle exported es module functions", () => {
   const ts = `export function routerReducer(state?: RouterState, action?: Action): RouterState;
 export function syncHistoryWithStore(history: History, store: Store<any>, options?: SyncHistoryWithStoreOptions): History & HistoryUnsubscribe;
 `;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
+
+it("should handle default exported es module functions", () => {
+  const ts = `export default function routerReducer(state?: RouterState, action?: Action): RouterState;`;
   const result = compiler.compileDefinitionString(ts);
   expect(beautify(result)).toMatchSnapshot();
 });
