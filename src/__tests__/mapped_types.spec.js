3724afcd7695c845ec3ef8bac6bcd9d4aa8f0df5
andretshurotshka <andwebar@gmail.com>
2019-02-03 02:28:56 +0500

    Add mapped types test

---
+++ src/__tests__/mapped_types.spec.js
@@ -0,0 +1,20 @@
+import { compiler, beautify } from "..";
+
+it("should handle mapped types", () => {
+  const ts = `
+type Ref<T> = {current: T | null}
+type SourceUnion = 'a' | 'b' | 'c'
+type SourceObject = {
+  a: number,
+  d: string
+}
+type MappedUnion = {
+  [K in SourceUnion]: Ref<number>
+}
+type MappedObj = {
+  [K in keyof SourceObject]: Ref<SourceObject[K]>
+}
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
