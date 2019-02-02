bb7eea2cba1113ca94e54288b5c6b09c9387c40a
andretshurotshka <andwebar@gmail.com>
2019-02-03 02:28:25 +0500

    Update enums test

--- src/__tests__/enums.spec.js
+++ src/__tests__/enums.spec.js
@@ -1,5 +1,11 @@
 import { compiler, beautify } from "..";
 
+it("should handle empty enums", () => {
+  const ts = `enum Empty { }`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
+
 it("should handle basic enums", () => {
   const ts = `enum Label {
     LABEL_OPTIONAL,
@@ -12,9 +18,11 @@ it("should handle basic enums", () => {
 
 it("should handle number enums", () => {
   const ts = `enum Label {
-    LABEL_OPTIONAL = 1,
-    LABEL_REQUIRED = 2,
-    LABEL_REPEATED = 3,
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    NEGATIVE = -123,
+    DECIMAL = 3.14,
   }`;
   const result = compiler.compileDefinitionString(ts);
   expect(beautify(result)).toMatchSnapshot();
