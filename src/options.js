4f32031f4e56104a603a44c98aebb6d722d7964a
andretshurotshka <andwebar@gmail.com>
2019-02-04 04:02:33 +0500

    Add options module

---
+++ src/options.js
@@ -0,0 +1,27 @@
+//@flow
+
+export type Options = {|
+  jsdoc: boolean,
+  interfaceRecords: boolean,
+  stringEnums: boolean,
+|};
+
+const defaultOptions: Options = Object.freeze({
+  jsdoc: true,
+  interfaceRecords: false,
+  stringEnums: false,
+});
+
+let options: Options = { ...defaultOptions };
+
+export function assignOptions(newOptions: $Shape<Options>) {
+  Object.assign(options, newOptions);
+}
+
+export function resetOptions() {
+  Object.assign(options, defaultOptions);
+}
+
+export function opts(): Options {
+  return options;
+}
