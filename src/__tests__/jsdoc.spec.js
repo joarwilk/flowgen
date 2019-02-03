bc8ae9eafb9f563eb8110323f86bca9b20cfe3be
andretshurotshka <andwebar@gmail.com>
2019-02-04 04:03:36 +0500

    Add jsdoc tests

---
+++ src/__tests__/jsdoc.spec.js
@@ -0,0 +1,30 @@
+import { compiler, beautify } from "..";
+
+it("should handle basic jsdoc", () => {
+  const ts = `const skip: number
+/**
+ * @param {string} userToken User authentication token
+ * @returns {Promise<void>}
+ */
+declare function authorize(userToken: string): Promise<void>
+
+/**
+ * Plain comment
+ */
+declare function test(): Promise<void>
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
+
+it("should remove jsdoc", () => {
+  const ts = `
+/**
+ * @param {string} userToken User authentication token
+ * @returns {Promise<void>}
+ */
+declare function authorize(userToken: string): Promise<void>
+`;
+  const result = compiler.compileDefinitionString(ts, { jsdoc: false });
+  expect(beautify(result)).toMatchSnapshot();
+});
