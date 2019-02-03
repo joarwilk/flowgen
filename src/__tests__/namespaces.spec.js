48e21428183507d1a39006ec25bab8c3d237c964
andretshurotshka <andwebar@gmail.com>
2019-02-04 04:03:51 +0500

    Add namespace tests

---
+++ src/__tests__/namespaces.spec.js
@@ -0,0 +1,37 @@
+import { compiler, beautify } from "..";
+
+it("should handle namespaces", () => {
+  const ts = `
+namespace test {
+  export const ok: number
+}
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
+
+it("should handle namespace merging", () => {
+  const ts = `
+namespace test {
+  export const ok: number
+}
+namespace test {
+  export const error: string
+}
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
+
+it("should handle namespace function merging", () => {
+  const ts = `
+namespace test {
+  declare function test(err: number): void
+}
+namespace test {
+  declare function test(response: string): string
+}
+`;
+  const result = compiler.compileDefinitionString(ts);
+  expect(beautify(result)).toMatchSnapshot();
+});
