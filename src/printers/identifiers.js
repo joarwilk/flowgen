a75985f64500adf548a5edf617863e3c37c3e55e
andretshurotshka <andwebar@gmail.com>
2019-02-04 06:38:51 +0500

    Add Partial, Readonly, NonNullable identifiers replacements

--- src/printers/identifiers.js
+++ src/printers/identifiers.js
@@ -1,7 +1,12 @@
 /* @flow */
 
+// Please add only built-in type references
+
 const identifiers = {
   ReadonlyArray: "$ReadOnlyArray",
+  Partial: "$Shape",
+  Readonly: "$ReadOnly",
+  NonNullable: "$NonMaybeType",
 };
 
 export const print = (kind: string): string => {
