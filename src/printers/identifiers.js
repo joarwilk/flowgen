aace662568bcba06a4099d0f46be9e16a55b2df4
andretshurotshka <andwebar@gmail.com>
2019-02-06 22:37:11 +0500

    Use Object.create(null)

--- src/printers/identifiers.js
+++ src/printers/identifiers.js
@@ -2,19 +2,14 @@
 
 // Please add only built-in type references
 
-const identifiers = {
+const identifiers = Object.create(null);
+Object.assign(identifiers, {
   ReadonlyArray: "$ReadOnlyArray",
   Partial: "$Shape",
   Readonly: "$ReadOnly",
   NonNullable: "$NonMaybeType",
-};
-
-// TODO: Object.entries should be fixed when Flow will switch to exact by default
-// We use Map, so it doesn't fail with `toString` identifier
-const identifiersMap: Map<string, string> = new Map(
-  (Object.entries(identifiers): any),
-);
+});
 
 export const print = (kind: string): string => {
-  return identifiersMap.get(kind) || kind;
+  return identifiers[kind] || kind;
 };
