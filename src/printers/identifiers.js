8597ef5143038e290fc2aefe196d02ae672c756f
andretshurotshka <andwebar@gmail.com>
2019-02-06 22:24:16 +0500

    Use Map instead of Object with prototype

--- src/printers/identifiers.js
+++ src/printers/identifiers.js
@@ -9,6 +9,12 @@ const identifiers = {
   NonNullable: "$NonMaybeType",
 };
 
+// TODO: Object.entries should be fixed when Flow will switch to exact by default
+// We use Map, so it doesn't fail with `toString` identifier
+const identifiersMap: Map<string, string> = new Map(
+  (Object.entries(identifiers): any),
+);
+
 export const print = (kind: string): string => {
-  return identifiers[kind] || kind;
+  return identifiersMap.get(kind) || kind;
 };
