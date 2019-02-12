4ac5fb2ea1fd039834b0bc8a357ac049d9191f2a
andretshurotshka <andwebar@gmail.com>
2019-02-12 10:05:00 +0500

    Support custom flow-typed directory

--- src/cli/flow-typed-exporter.js
+++ src/cli/flow-typed-exporter.js
@@ -1,15 +1,20 @@
 // @flow
+
 import fs from "fs";
 import shell from "shelljs";
+import program from "commander";
 
 export default function exportForFlowTyped(
   moduleName: string,
   output: string,
 ): string {
-  const folder = "../exports/" + moduleName + "_v1.x.x";
-  const outputFile = folder + "/flow_v0.35.x-/" + moduleName + ".js";
+  const opts = program.opts();
+  const out =
+    typeof opts.flowTypedFormat === "string" ? opts.flowTypedFormat : "exports";
+  const folder = `./${out}/${moduleName}_v1.x.x`;
+  const outputFile = `${folder}/flow_v0.35.x-/${moduleName}.js`;
 
-  const testfilePath = folder + "/test_" + moduleName + ".js";
+  const testfilePath = `${folder}/test_${moduleName}.js`;
 
   if (!fs.existsSync(folder)) {
     shell.mkdir("-p", folder);
