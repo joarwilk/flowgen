d82ae18907ca701e075e742ef44c3fb2fca3ba80
andretshurotshka <andwebar@gmail.com>
2019-02-12 10:10:31 +0500

    Add support for nested values

--- src/nodes/namespace.js
+++ src/nodes/namespace.js
@@ -1,4 +1,6 @@
 /* @flow */
+
+import * as ts from "typescript";
 import { uniqBy, flatten } from "lodash";
 import Node from "./node";
 
@@ -16,40 +18,112 @@ export default class Namespace extends Node {
 
   addChild(name: string, child: Node<>): void {
     child.namespace = this.name;
+    child.isValue = child
+      .getChildren()
+      .some(
+        node =>
+          node instanceof Namespace ||
+          node.raw.kind === ts.SyntaxKind.VariableStatement ||
+          node.raw.kind === ts.SyntaxKind.ClassDeclaration ||
+          node.raw.kind === ts.SyntaxKind.FunctionDeclaration ||
+          node.raw.kind === ts.SyntaxKind.EnumDeclaration,
+      );
     namespaceManager.registerProp(this.name, child.name);
 
     this.children[name] = child;
   }
 
-  print = (): string => {
+  addChildren(name: string, child: Node<>): void {
+    child.namespace = this.name;
+    child.isValue = child
+      .getChildren()
+      .some(
+        node =>
+          node instanceof Namespace ||
+          node.raw.kind === ts.SyntaxKind.VariableStatement ||
+          node.raw.kind === ts.SyntaxKind.ClassDeclaration ||
+          node.raw.kind === ts.SyntaxKind.FunctionDeclaration ||
+          node.raw.kind === ts.SyntaxKind.EnumDeclaration,
+      );
+    namespaceManager.registerProp(this.name, child.name);
+
+    if (!this.children[name]) {
+      this.children[name] = child;
+      return;
+    }
+    if (this.children[name]) {
+      for (const key in child.children) {
+        this.children[name].addChild(key, child.children[key]);
+      }
+      return;
+    }
+  }
+
+  print = (namespace: string = ""): string => {
     const functions = this.getChildren().filter(
-      child => child.raw && child.raw.kind === "FunctionDeclaration",
+      child =>
+        child.raw && child.raw.kind === ts.SyntaxKind.FunctionDeclaration,
     );
     const variables = flatten(
       this.getChildren()
-        .filter(child => child.raw && child.raw.kind === "VariableStatement")
+        .filter(
+          child =>
+            child.raw && child.raw.kind === ts.SyntaxKind.VariableStatement,
+        )
         .map(child => child.raw.declarationList.declarations),
     );
+    const enums = this.getChildren().filter(
+      child => child.raw && child.raw.kind === ts.SyntaxKind.EnumDeclaration,
+    );
+    const classes = this.getChildren().filter(
+      child => child.raw && child.raw.kind === ts.SyntaxKind.ClassDeclaration,
+    );
+    const namespaces = this.getChildren().filter(child => {
+      return child.isValue;
+    });
+    let name = this.name;
+    if (namespace) {
+      name = namespace + "$" + this.name;
+    }
 
     const children = `${this.getChildren()
       .map(child => {
-        return child.print(this.name);
+        return child.print(name);
       })
       .join("\n\n")}`;
 
-    if (functions.length || variables.length) {
+    if (
+      functions.length ||
+      variables.length ||
+      namespaces.length ||
+      classes.length ||
+      enums.length
+    ) {
       const nsGroup = `
-      declare var npm$namespace$${this.name}: {
+      declare var npm$namespace$${name}: {
         ${uniqBy(functions, child => child.name)
           .map(child => {
-            return `${child.name}: typeof ${this.name}$${child.name},`;
+            return `${child.name}: typeof ${name}$${child.name},`;
           })
           .join("\n")}
         ${variables
           .map(child => {
-            return `${child.name.text}: typeof ${this.name}$${
-              child.name.text
-            },`;
+            return `${child.name.text}: typeof ${name}$${child.name.text},`;
+          })
+          .join("\n")}
+        ${enums
+          .map(child => {
+            return `${child.name}: typeof ${name}$${child.name},`;
+          })
+          .join("\n")}
+        ${classes
+          .map(child => {
+            return `${child.name}: typeof ${name}$${child.name},`;
+          })
+          .join("\n")}
+        ${namespaces
+          .map(child => {
+            return `${child.name}: typeof npm$namespace$${name}$${child.name},`;
           })
           .join("\n")}
       }\n`;
