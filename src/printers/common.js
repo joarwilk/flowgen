488ed4607b4a3f4c70f00c3d8b352afd9aa182a7
andretshurotshka <andwebar@gmail.com>
2019-02-14 20:04:19 +0500

    Move literal and typeParameter to printers

--- src/printers/common.js
+++ src/printers/common.js
@@ -1,27 +1,59 @@
 /* @flow */
 
+import * as ts from "typescript";
 import { opts } from "../options";
 import type { RawNode } from "../nodes/node";
 
 import printers from "./index";
 
+export const literalType = (node: RawNode) => {
+  if (node.literal) {
+    if (node.literal.kind === ts.SyntaxKind.StringLiteral) {
+      return printers.node.printType(node.literal);
+    } else if (node.literal.text) {
+      return node.literal.text;
+    } else {
+      return printers.node.printType(node.literal);
+    }
+  }
+
+  if (node.type.typeName) {
+    return node.type.typeName.text;
+  }
+  return printers.node.printType(node.type);
+};
+
+export const typeParameter = (
+  node: ts.TypeParameterDeclaration & { withoutDefault: boolean },
+) => {
+  let defaultType = "";
+  let constraint = "";
+  if (node.constraint) {
+    constraint = `: ${printers.node.printType(node.constraint)}`;
+  }
+  if (!node.withoutDefault && node.default) {
+    defaultType = `= ${printers.node.printType(node.default)}`;
+  }
+  return `${node.name.text}${constraint}${defaultType}`;
+};
+
 export const parameter = (param: RawNode): string => {
   let left = "";
   if (param.modifiers) {
     for (const modifier of param.modifiers) {
-      if (modifier.kind === "ReadonlyKeyword") left += "+";
+      if (modifier.kind === ts.SyntaxKind.ReadonlyKeyword) left += "+";
     }
   }
   let right;
 
-  if (param.name.kind === "ObjectBindingPattern") {
+  if (param.name.kind === ts.SyntaxKind.ObjectBindingPattern) {
     left = `x`;
   } else {
     left += printers.node.printType(param.name);
   }
 
   if (!param.type) {
-    if (param.name.kind === "ObjectBindingPattern") {
+    if (param.name.kind === ts.SyntaxKind.ObjectBindingPattern) {
       if (param.name.elements.length > 0) {
         right = `{${param.name.elements
           .map(element => `${printers.node.printType(element)}: any`)
@@ -52,7 +84,7 @@ export const methodSignature = (param: RawNode) => {
   let isMethod = true;
   if (param.modifiers) {
     for (const modifier of param.modifiers) {
-      if (modifier.kind === "ReadonlyKeyword") {
+      if (modifier.kind === ts.SyntaxKind.ReadonlyKeyword) {
         left += "+";
         isMethod = false;
       }
@@ -82,7 +114,7 @@ export const parseTypeReference = (node: RawNode): string => {
 };
 
 export const generics = (
-  types: ?Array<RawNode>,
+  types: ?$ReadOnlyArray<RawNode>,
   map?: (node: RawNode) => RawNode = node => node,
 ): string => {
   if (types && types.length) {
@@ -112,7 +144,9 @@ export const comment = (jsdoc: Array<any>): string => {
         const parameterName = (tag.name || tag.preParameterName || {}).text
           ? ` ${(tag.name || tag.preParameterName || {}).text}`
           : "";
-        const comment = tag.comment ? ` ${tag.comment}` : "";
+        const comment = tag.comment
+          ? ` ${tag.comment}`.replace(/\n/g, "\n * ")
+          : "";
         return `\n * @${tag.tagName.text}${typeName}${parameterName}${comment}`;
       });
 
