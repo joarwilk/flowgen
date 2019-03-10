bbe3e54766cead9acded2d7f5e97a4d8d95cefed
andretshurotshka <andwebar@gmail.com>
2019-03-10 08:47:14 +0500

    Update utility types test with overshadowing

--- src/__tests__/utility_types.spec.js
+++ src/__tests__/utility_types.spec.js
@@ -8,6 +8,19 @@ type A = Readonly<{a: number}>
 type B = Partial<{a: number}>
 type C = NonNullable<string | null>
 type D = ReadonlyArray<string>
+type E = ReturnType<() => string>
+
+type A1<Readonly> = Readonly
+type B1<Partial> = Partial
+type C1<NonNullable> = NonNullable
+type D1<ReadonlyArray> = ReadonlyArray
+type E1<ReturnType> = ReturnType
+
+type A2<T> = Readonly<T>
+type B2<T> = Partial<T>
+type C2<T> = NonNullable<T>
+type D2<T> = ReadonlyArray<T>
+type E2<T> = ReturnType<() => T>
 `;
   const result = compiler.compileDefinitionString(ts);
   expect(beautify(result)).toMatchSnapshot();
