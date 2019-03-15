1f63669e8fdec892b835568f93b03285215e11fa
andretshurotshka <andwebar@gmail.com>
2019-03-15 19:53:53 +0500

    Make async default exporter

--- src/cli/default-exporter.js
+++ src/cli/default-exporter.js
@@ -3,19 +3,31 @@
 import fs from "fs";
 import shell from "shelljs";
 import path from "path";
+import { promisify } from "./util";
+
+const open: (
+  filename: string,
+  flags: string | number,
+) => Promise<number> = promisify(fs.open);
+const writeFile: (fd: number, output: string) => Promise<void> = promisify(
+  fs.writeFile,
+);
 
 /**
  * Takes a path and some content and performs a write call. Simple.
  */
-export default function exportDefault(
+export default async function exportDefault(
   fileName: string,
   output: string,
-): string {
+): Promise<string> {
   const folderName = path.dirname(fileName);
-  if (!fs.existsSync(folderName)) {
+  let handle;
+  try {
+    handle = await open(fileName, "w");
+  } catch {
     shell.mkdir("-p", folderName);
+    handle = await open(fileName, "w");
   }
-  fs.writeFileSync(fileName, output);
-
+  await writeFile(handle, output);
   return fileName;
 }
