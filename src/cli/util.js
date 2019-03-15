a8d757350a1935e868277df73a914bd3482f71f2
andretshurotshka <andwebar@gmail.com>
2019-03-15 19:55:35 +0500

    Add promisify typed wrapper

--- src/cli/util.js
+++ src/cli/util.js
@@ -3,39 +3,45 @@
 import util from "util";
 
 // TODO: pull request promisify types to flow
+// TODO: fix types for multiple arguments
 
 declare export function promisify<TResult>(
-  fn: (callback: (err: ?Error, result: TResult) => void) => void,
+  fn: (callback: (err: ?ErrnoError, result: TResult) => void) => void,
 ): () => Promise<TResult>;
 declare export function promisify(
-  fn: (callback: (err?: ?Error) => void) => void,
+  fn: (callback: (err: ?ErrnoError) => void) => void,
 ): () => Promise<void>;
 declare export function promisify<T1, TResult>(
-  fn: (arg1: T1, callback: (err: ?Error, result: TResult) => void) => void,
+  fn: (arg1: T1, callback: (err: ?ErrnoError, result: TResult) => void) => void,
 ): (arg1: T1) => Promise<TResult>;
 declare export function promisify<T1>(
-  fn: (arg1: T1, callback: (err?: ?Error) => void) => void,
+  fn: (arg1: T1, callback: (err: ?ErrnoError) => void) => void,
 ): (arg1: T1) => Promise<void>;
 declare export function promisify<T1, T2, TResult>(
   fn: (
     arg1: T1,
     arg2: T2,
-    callback: (err: ?Error, result: TResult) => void,
+    callback: (err: ?ErrnoError, result: TResult) => void,
   ) => void,
 ): (arg1: T1, arg2: T2) => Promise<TResult>;
 declare export function promisify<T1, T2>(
-  fn: (arg1: T1, arg2: T2, callback: (err?: ?Error) => void) => void,
+  fn: (arg1: T1, arg2: T2, callback: (err: ?ErrnoError) => void) => void,
 ): (arg1: T1, arg2: T2) => Promise<void>;
 declare export function promisify<T1, T2, T3, TResult>(
   fn: (
     arg1: T1,
     arg2: T2,
     arg3: T3,
-    callback: (err: ?Error, result: TResult) => void,
+    callback: (err: ?ErrnoError, result: TResult) => void,
   ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>;
 declare export function promisify<T1, T2, T3>(
-  fn: (arg1: T1, arg2: T2, arg3: T3, callback: (err?: ?Error) => void) => void,
+  fn: (
+    arg1: T1,
+    arg2: T2,
+    arg3: T3,
+    callback: (err: ?ErrnoError) => void,
+  ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3) => Promise<void>;
 declare export function promisify<T1, T2, T3, T4, TResult>(
   fn: (
@@ -43,7 +49,7 @@ declare export function promisify<T1, T2, T3, T4, TResult>(
     arg2: T2,
     arg3: T3,
     arg4: T4,
-    callback: (err: ?Error, result: TResult) => void,
+    callback: (err: ?ErrnoError, result: TResult) => void,
   ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>;
 declare export function promisify<T1, T2, T3, T4>(
@@ -52,7 +58,7 @@ declare export function promisify<T1, T2, T3, T4>(
     arg2: T2,
     arg3: T3,
     arg4: T4,
-    callback: (err?: ?Error) => void,
+    callback: (err: ?ErrnoError) => void,
   ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<void>;
 declare export function promisify<T1, T2, T3, T4, T5, TResult>(
@@ -62,7 +68,7 @@ declare export function promisify<T1, T2, T3, T4, T5, TResult>(
     arg3: T3,
     arg4: T4,
     arg5: T5,
-    callback: (err: ?Error, result: TResult) => void,
+    callback: (err: ?ErrnoError, result: TResult) => void,
   ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => Promise<TResult>;
 declare export function promisify<T1, T2, T3, T4, T5>(
@@ -72,7 +78,7 @@ declare export function promisify<T1, T2, T3, T4, T5>(
     arg3: T3,
     arg4: T4,
     arg5: T5,
-    callback: (err?: ?Error) => void,
+    callback: (err: ?ErrnoError) => void,
   ) => void,
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => Promise<void>;
 declare export function promisify(f: Function): Function;
