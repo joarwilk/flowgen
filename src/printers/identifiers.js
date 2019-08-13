28553e6dd9d49eaa11d80d92cf1a699f4b848b85
andretshurotshka <andwebar@gmail.com>
2019-08-14 00:35:00 +0500

    Add missing inexact objects

--- src/printers/identifiers.js
+++ src/printers/identifiers.js
@@ -3,6 +3,7 @@
 // Please add only built-in type references
 
 import printers from "./index";
+import { opts } from "../options";
 import { withEnv } from "../env";
 
 const identifiers = Object.create(null);
@@ -13,17 +14,22 @@ Object.assign(identifiers, {
   Readonly: "$ReadOnly",
   NonNullable: "$NonMaybeType",
   Partial: ([type]: any[]) => {
-    return `$Rest<${printers.node.printType(type)}, {}>`;
+    const isInexact = opts().inexact;
+    return `$Rest<${printers.node.printType(type)}, {${
+      isInexact ? "..." : ""
+    }}>`;
   },
   ReturnType: (typeArguments: any[]) => {
     return `$Call<<R>((...args: any[]) => R) => R, ${printers.node.printType(
       typeArguments[0],
     )}>`;
   },
-  Record: ([key, value]: [any, any]) =>
-    `{[key: ${printers.node.printType(key)}]: ${printers.node.printType(
+  Record: ([key, value]: [any, any]) => {
+    const isInexact = opts().inexact;
+    return `{[key: ${printers.node.printType(key)}]: ${printers.node.printType(
       value,
-    )}}`,
+    )}${isInexact ? ", ..." : ""}}`;
+  },
 });
 
 export const print = withEnv<any, [string], string>(
