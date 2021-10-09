b851545eed20a76f341f2a1bc2a23eda959b388e
Moroine <moroine.bentefrit@gmail.com>
2021-10-10 01:38:11 +0200

    fix: support generic type rename

--- src/__tests__/duplicated-names.spec.ts
+++ src/__tests__/duplicated-names.spec.ts
@@ -38,3 +38,19 @@ it("should be scoped to main file", () => {
   // FlowJs is not valid due to `import` not transpiled to typeof
   // expect(result).toBeValidFlowTypeDeclarations();
 });
+
+it("should support generic type rename", () => {
+  const ts = `
+export declare const ProfilingLevel: Readonly<{
+  readonly off: "off";
+}>;
+export declare type ProfilingLevel = typeof ProfilingLevel[keyof typeof ProfilingLevel];
+
+export declare type Callback<T = any> = (error?: Error, result?: T) => void;
+
+export declare const callback: Callback<ProfilingLevel>;
+`;
+  const result = compiler.compileDefinitionString(ts, { quiet: true });
+  expect(beautify(result)).toMatchSnapshot();
+  expect(result).toBeValidFlowTypeDeclarations();
+});
