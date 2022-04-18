fad4e4b149c02951ef14d7b42dc03da15870003b
Greg Price <greg@zulip.com>
2022-04-18 12:39:29 -0700

    printers [nfc]: Split classBody from interfaceType
    This function has three call sites.  One of them has needs that are
    fairly divergent from the needs of the other two.  So, split the
    function in two, and make both sides simpler.

--- src/printers/declarations.ts
+++ src/printers/declarations.ts
@@ -76,15 +76,34 @@ export const typeMembers = (
     .filter(Boolean); // Filter rows which didn't print properly (private fields et al)
 };
 
-export const interfaceType = <T>(
-  node: ts.InterfaceDeclaration | ts.ClassDeclaration | ts.TypeLiteralNode,
-  nodeName: string,
-  mergedNamespaceChildren: ReadonlyArray<Node<T>>,
-  withSemicolons = false,
+export const interfaceType = (
+  node: ts.InterfaceDeclaration | ts.TypeLiteralNode,
   isType = false,
 ): string => {
   const isInexact = opts().inexact;
 
+  const members = typeMembers(node);
+
+  if (isType && isInexact) {
+    members.push("...\n");
+  } else if (members.length > 0) {
+    members.push("\n");
+  }
+
+  const inner = members.join(",");
+
+  // we only want type literals to be exact. i.e. class Foo {} should not be class Foo {||}
+  if (!ts.isTypeLiteralNode(node)) {
+    return `{${inner}}`;
+  }
+  return isInexact ? `{${inner}}` : `{|${inner}|}`;
+};
+
+const classBody = <T>(
+  node: ts.ClassDeclaration,
+  nodeName: string,
+  mergedNamespaceChildren: ReadonlyArray<Node<T>>,
+): string => {
   const members = typeMembers(node);
 
   if (mergedNamespaceChildren.length > 0) {
@@ -96,19 +115,13 @@ export const interfaceType = <T>(
     }
   }
 
-  if (isType && isInexact) {
-    members.push("...\n");
-  } else if (members.length > 0) {
+  if (members.length > 0) {
     members.push("\n");
   }
 
-  const inner = members.join(withSemicolons ? ";" : ",");
+  const inner = members.join(";");
 
-  // we only want type literals to be exact. i.e. class Foo {} should not be class Foo {||}
-  if (!ts.isTypeLiteralNode(node)) {
-    return `{${inner}}`;
-  }
-  return isInexact ? `{${inner}}` : `{|${inner}|}`;
+  return `{${inner}}`;
 };
 
 const interfaceRecordType = (
@@ -228,9 +241,6 @@ export const interfaceDeclaration = (
     node.typeParameters,
   )} ${type === "type" ? "= " : ""}${interfaceType(
     node,
-    nodeName,
-    [],
-    false,
     type === "type",
   )} ${heritage}`;
 
@@ -317,12 +327,7 @@ export const classDeclaration = <T>(
     node,
   )}class ${nodeName}${printers.common.generics(
     node.typeParameters,
-  )} ${heritage} ${interfaceType(
-    node,
-    nodeName,
-    mergedNamespaceChildren,
-    true,
-  )}`;
+  )} ${heritage} ${classBody(node, nodeName, mergedNamespaceChildren)}`;
 
   return str;
 };
--- src/printers/node.ts
+++ src/printers/node.ts
@@ -498,7 +498,7 @@ export const printType = withEnv<any, [any], string>(
         return printers.functions.functionType(type);
 
       case ts.SyntaxKind.TypeLiteral:
-        return printers.declarations.interfaceType(type, "", [], false, true);
+        return printers.declarations.interfaceType(type, true);
 
       //case SyntaxKind.IdentifierObject:
       //case SyntaxKind.StringLiteralType:
