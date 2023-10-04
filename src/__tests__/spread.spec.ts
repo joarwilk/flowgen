8c4db6309803f55c8fd27e0b771a322c9702a4cf
Dominic Tobias <dominic.tobias@bitmex.com>
2023-10-04 23:29:34 +0100

    fix: snaps

--- src/__tests__/spread.spec.ts
+++ src/__tests__/spread.spec.ts
@@ -7,21 +7,12 @@ type Foo = { foo: number };
 type Bar = { bar: string };
 const combination: Foo & Bar;
 `;
-
-  {
-    const result = compiler.compileDefinitionString(ts, { quiet: true });
-    expect(beautify(result)).toMatchSnapshot();
-    expect(result).toBeValidFlowTypeDeclarations();
-  }
-
-  {
-    const result = compiler.compileDefinitionString(ts, {
-      quiet: true,
-      inexact: false,
-    });
-    expect(beautify(result)).toMatchSnapshot();
-    expect(result).toBeValidFlowTypeDeclarations();
-  }
+  const result = compiler.compileDefinitionString(ts, {
+    quiet: true,
+    inexact: false,
+  });
+  expect(beautify(result)).toMatchSnapshot();
+  expect(result).toBeValidFlowTypeDeclarations();
 });
 
 it("should not use spread when performing union of inexact object types", () => {
@@ -30,21 +21,12 @@ type Foo = { foo: number };
 type Bar = { bar: string };
 const combination: Foo & Bar;
 `;
-
-  {
-    const result = compiler.compileDefinitionString(ts, { quiet: true });
-    expect(beautify(result)).toMatchSnapshot();
-    expect(result).toBeValidFlowTypeDeclarations();
-  }
-
-  {
-    const result = compiler.compileDefinitionString(ts, {
-      quiet: true,
-      inexact: true,
-    });
-    expect(beautify(result)).toMatchSnapshot();
-    expect(result).toBeValidFlowTypeDeclarations();
-  }
+  const result = compiler.compileDefinitionString(ts, {
+    quiet: true,
+    inexact: true,
+  });
+  expect(beautify(result)).toMatchSnapshot();
+  expect(result).toBeValidFlowTypeDeclarations();
 });
 
 it("should not insert spread when performing union of class types", () => {
